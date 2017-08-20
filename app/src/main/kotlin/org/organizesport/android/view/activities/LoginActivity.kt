4cf7773d64894e787cb8dfde162f7082499da8cf
Pablo L <pablo.sordomartinez@unitedcuisines.de>
2017-08-20 18:48:58 +0200

    Added 'LoginActivity' and its related layout file

---
+++ app/src/main/kotlin/org/organizesport/android/view/activities/LoginActivity.kt
@@ -0,0 +1,14 @@
+package org.organizesport.android.view.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import org.organizesport.android.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
